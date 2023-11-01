bc0112ebe0bc6d4bdc33234aad95eabee2c79aae
Refia <112306763+refiaa@users.noreply.github.com>
2023-11-01 23:02:52 +0900

    Update ncPlotter.py

--- scr/ncPlotter.py
+++ scr/ncPlotter.py
@@ -4,10 +4,12 @@ import netCDF4 as nc
 import numpy as np
 import os
 import imageio.v2 as imageio
-    
+
+from io import BytesIO
 from shapely.geometry import box
 from datetime import datetime, timedelta
 from PIL import Image
+
 
 ##################################### DESCRIPTION #########################################
 #                                                                                         #
@@ -80,60 +82,54 @@ def wrap_data(lon, lat, data, lon_min, lon_max, lat_min, lat_max):
     return wrapped_lon[sorted_indices_lon][lon_mask], lat_1d[lat_mask], wrapped_data
 
 def crop_map(data, lon_min, lon_max, lat_min, lat_max):
-    
     return data.cx[lon_min:lon_max, lat_min:lat_max]
 
-def create_gif(nc_file, gif_file_path, lon_min, lon_max, lat_min, lat_max, start_date, end_date, resolution, transparency, max_value, color_map, frame_duration, variable_name):
+def gifIO(nc_file, gif_file_path, lon_min, lon_max, lat_min, lat_max, start_date, end_date, resolution, transparency, max_value, color_map, frame_duration, variable_name):
     dataset = nc.Dataset(nc_file)
     lon = dataset.variables['lon'][:]
     lat = dataset.variables['lat'][:]
     data = dataset.variables[variable_name][:]
     
     lon, lat, data = wrap_data(lon, lat, data, lon_min, lon_max, lat_min, lat_max)
-    world_cropped = crop_map(world, lon_min, lon_max, lat_min, lat_max)
     
     frames = []
-
-    def resize_frame(frame, resolution):
-        img = Image.fromarray(frame)
-        img = img.resize(resolution)
-        
-        return np.array(img)
 
     start_date = datetime.strptime(start_date, '%Y/%m/%d')
     end_date = datetime.strptime(end_date, '%Y/%m/%d')
 
     for i in range(data.shape[0]):
         current_date = start_date + timedelta(days=i)
-        
+
         if current_date <= end_date:
             fig, ax = plt.subplots(figsize=(resolution[0] / 100, resolution[1] / 100))
-            world_cropped.boundary.plot(ax=ax, linewidth=1, color='k')
+            world.boundary.plot(ax=ax, linewidth=1, color='k')
 
             plt.imshow(data[i, ::-1, :], extent=(lon_min, lon_max, lat_min, lat_max), cmap=color_map, alpha=transparency, vmax=max_value)
-            
             plt.title(f'Time: {current_date.strftime("%Y/%m/%d")}')
             plt.colorbar(label=f'{variable_name} ({dataset.variables[variable_name].units})') 
-            plt.savefig('./plot/temp.png', dpi=resolution[2], bbox_inches='tight')  
-            plt.close()
 
-            frame = imageio.imread('./plot/temp.png')
-            frame = resize_frame(frame, resolution[:2])  
-            frames.append(frame)
+            with BytesIO() as buf:
+                plt.savefig(buf, format='png', dpi=resolution[2], bbox_inches='tight')
+                plt.close()
 
-    imageio.mimsave(gif_file_path, frames, duration=frame_duration)
-    os.remove('./plot/temp.png')
+                buf.seek(0)
+
+                frame = Image.open(buf)
+                frame = frame.resize(resolution[:2])
+                frames.append(frame)
+
+    frames[0].save(gif_file_path, save_all=True, append_images=frames[1:], duration=frame_duration, loop=0)
 
 try:
     world = gpd.read_file('./shp/World_Countries_Generalized.shp')
 
 except Exception as e:
-    print(f"Error on shp file: {str(e)}")
+    print(f"Fail to load shapefile: {str(e)}")
 
 BASE_NC_DIR = './nc/'
 BASE_GIF_DIR = './plot/'
 
-nc_file = os.path.join(BASE_NC_DIR, nc_filename) 
-gif_file_path = os.path.join(BASE_GIF_DIR, gif_filename) 
+nc_file = os.path.join(BASE_NC_DIR, nc_filename)
+gif_file_path = os.path.join(BASE_GIF_DIR, gif_filename)
 
-create_gif(nc_file, gif_file_path, lon_min, lon_max, lat_min, lat_max, start_date, end_date, resolution, transparency, max_value, color_map, frame_duration, variable_name)
+gifIO(nc_file, gif_file_path, lon_min, lon_max, lat_min, lat_max, start_date, end_date, resolution, transparency, max_value, color_map, frame_duration, variable_name)
